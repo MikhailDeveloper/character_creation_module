928faabb0ad3830320cab379e1177d60ca870d1b
Mikhail <63748477+MikhailDeveloper@users.noreply.github.com>
2023-02-08 15:54:22 +0300

    Update main.py
    Добавлены документирующие комментарии

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Возвращает f-строку, колличество урона противнику."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Возвращает f-строку, колличество блокированых атак."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +25,8 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Возвращает f-строку, прокаченные скилы."""
+    cmd = ''
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -42,6 +46,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Возвращает f-строку, выбраного персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -60,7 +65,6 @@ def choice_char_class() -> str:
         approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
                                'или любую другую кнопку, '
                                'чтобы выбрать другого персонажа ').lower()
-    return char_class
 
 
 if __name__ == '__main__':
